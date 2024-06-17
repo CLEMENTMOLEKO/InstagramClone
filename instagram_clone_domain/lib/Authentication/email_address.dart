f0b9e6ed576e8500f98b08256a618ae90b6105de
Clement Moleko <molekoclement@gmail.com>
2024-06-17 18:03:56 +0200

    added email address validated value object and publish to none on pubspec for every project, referenced domain in the application layer

---
+++ instagram_clone_domain/lib/Authentication/email_address.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:instagram_clone_domain/Authentication/Errors/failure.dart';
+import 'package:instagram_clone_domain/Models/value_object.dart';
+
+final class EmailAddress extends ValueObject {
+  final String value;
+  EmailAddress._({required this.value});
+
+  static Either<Failure, EmailAddress> create({required String email}) {
+    //TODO: validate email before creation
+    if (_isValidEmail(email)) {
+      return right(EmailAddress._(value: email));
+    }
+    return left(Failure.invalidEmail);
+  }
+
+  static bool _isValidEmail(String email) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(email);
+  }
+
+  @override
+  List<Object?> get props => [value];
+}
---
+++ instagram_clone_domain/test/Authentication/email_address_test.dart
@@ -0,0 +1,64 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:instagram_clone_domain/Authentication/Errors/failure.dart';
+import 'package:instagram_clone_domain/Authentication/email_address.dart';
+import 'package:instagram_clone_domain/Models/value_object.dart';
+
+void main() {
+  EmailAddress arrangeEmailAddress({email = 'test1@example.com'}) {
+    return EmailAddress.create(email: email)
+        .getOrElse(() => throw Exception('Invalid email'));
+  }
+
+  group('EmailAddress', () {
+    const testExample1Email = 'test1@example.com';
+    const testExample2Email = 'test2@example.com';
+    const invalidEmail = 'testeamil.com@com';
+
+    group("Create", () {
+      test("Should return an email address when provided a valid email address",
+          () {
+        //Arrange
+        //Act
+        final result = EmailAddress.create(email: testExample2Email);
+        //Assert
+        expect(result, Right(arrangeEmailAddress(email: testExample2Email)));
+      });
+      test("Should return failure when provided an invalid email address", () {
+        //Arrange
+        //Act
+        final result = EmailAddress.create(email: invalidEmail);
+        //Assert
+        expect(result, const Left(Failure.invalidEmail));
+      });
+    });
+    test('should implement ValueObject', () {
+      //Arrange
+      //Act
+      final email = arrangeEmailAddress();
+      //Assert
+      expect(email, isA<ValueObject>());
+    });
+
+    test('should be equal if two EmailAddress instances have the same value',
+        () {
+      //Arrange
+      //Act
+      final email1 = arrangeEmailAddress();
+      final email2 = arrangeEmailAddress();
+      //Assert
+      expect(email1, equals(email2));
+    });
+
+    test(
+        'should not be equal if two EmailAddress instances have different values',
+        () {
+      //Arrange
+      //Act
+      final email1 = arrangeEmailAddress(email: testExample1Email);
+      final email2 = arrangeEmailAddress(email: testExample2Email);
+      //Assert
+      expect(email1, isNot(equals(email2)));
+    });
+  });
+}
