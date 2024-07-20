bddad2df89637efdd6647ec2ef0ebd36fced504f
Clement Moleko <molekoclement@gmail.com>
2024-07-20 18:54:54 +0200

    remove unused imports

--- instagram_clone_infrastructure/test/user/firebase_user_repository_test.dart
+++ instagram_clone_infrastructure/test/user/firebase_user_repository_test.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:instagram_clone_application/instagram_clone_application.dart';
 import 'package:instagram_clone_application/user/user_repository.dart';
