1a84105321d0f4151f7241283866afb53c69e986
Clement Moleko <molekoclement@gmail.com>
2024-08-19 21:45:44 +0200

    Add tests to test if body is a gradient view.

--- instagram_clone_presentation/test/authentication/sign_up/views/sign_up_email_view_test.dart
+++ instagram_clone_presentation/test/authentication/sign_up/views/sign_up_email_view_test.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:instagram_clone_presentation/authentication/sign_up/views/sign_up_email_view.dart';
+import 'package:instagram_clone_presentation/common/widgets/gradient_view.dart';
 import 'package:instagram_clone_presentation/common/widgets/insta_text_field.dart';
 
 void main() {
@@ -23,5 +24,16 @@ void main() {
     //Assert
     expect(instaTextFieldFinder, findsOneWidget);
     expect(instaTextField.labelText, "Email Address");
+  });
+
+  testWidgets("Should render GradientView as its body", (widgetTester) async {
+    //Arrange
+    await pumpSignUpEmailView(widgetTester);
+    final scaffoldFinder = find.byType(Scaffold);
+    final scaffold = widgetTester.widget(scaffoldFinder) as Scaffold;
+    //Act
+    //Assert
+    expect(scaffoldFinder, findsOneWidget);
+    expect(scaffold.body, isA<GradientView>());
   });
 }
