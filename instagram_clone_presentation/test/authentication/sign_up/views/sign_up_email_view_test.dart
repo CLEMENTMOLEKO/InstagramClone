b241832a3735dc0c3e833fed4093484035973f4b
Clement Moleko <molekoclement@gmail.com>
2024-08-19 21:41:31 +0200

    AAA

--- instagram_clone_presentation/test/authentication/sign_up/views/sign_up_email_view_test.dart
+++ instagram_clone_presentation/test/authentication/sign_up/views/sign_up_email_view_test.dart
@@ -14,10 +14,13 @@ void main() {
 
   testWidgets("Should render instatextfield with Email Address label",
       (widgetTester) async {
+    //Arrange
     await pumpSignUpEmailView(widgetTester);
     final instaTextFieldFinder = find.byType(InstaTextField);
     final instaTextField =
         widgetTester.widget(instaTextFieldFinder) as InstaTextField;
+    //Act
+    //Assert
     expect(instaTextFieldFinder, findsOneWidget);
     expect(instaTextField.labelText, "Email Address");
   });
