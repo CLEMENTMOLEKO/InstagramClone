f6084f3e84e9cc8dab50a1d33a8497cefbfa864e
Clement Moleko <molekoclement@gmail.com>
2024-11-26 22:03:35 +0200

    adjust profile header.

--- instagram_clone_presentation/lib/home/widgets/profile_header.dart
+++ instagram_clone_presentation/lib/home/widgets/profile_header.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ProfileHeader extends StatelessWidget {
   const ProfileHeader({super.key});
@@ -10,27 +12,55 @@ class ProfileHeader extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Padding(
-          padding: EdgeInsets.only(left: 12),
-          child: CircleAvatar(
-            radius: 40,
-            backgroundImage: CachedNetworkImageProvider(
-              "http://via.placeholder.com/350x150",
-            ),
+        Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: Column(
+            children: [
+              const Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: CachedNetworkImageProvider(
+                      "http://via.placeholder.com/350x150",
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Icon(
+                      Icons.add_circle_rounded,
+                      color: CupertinoColors.link,
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(8),
+              Text("Username",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontWeight: FontWeight.w600)),
+            ],
           ),
         ),
-        _buildProfileStatsCount(
-          context: context,
-          count: "0",
-          label: "Posts",
-        ),
-        _buildProfileStatsCount(
-          context: context,
-          count: "0",
-          label: "Followers",
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: _buildProfileStatsCount(
+            context: context,
+            count: "0",
+            label: "Posts",
+          ),
         ),
         Padding(
-          padding: const EdgeInsets.only(right: 16),
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: _buildProfileStatsCount(
+            context: context,
+            count: "0",
+            label: "Followers",
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 16, top: 8),
           child: _buildProfileStatsCount(
             context: context,
             count: "0",
@@ -50,15 +80,15 @@ class ProfileHeader extends StatelessWidget {
       children: [
         Text(
           count,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
                 fontWeight: FontWeight.w600,
               ),
         ),
         Text(label,
             style: Theme.of(context)
                 .textTheme
-                .bodyMedium
-                ?.copyWith(fontWeight: FontWeight.w600)),
+                .titleMedium
+                ?.copyWith(fontWeight: FontWeight.w500)),
       ],
     );
   }
